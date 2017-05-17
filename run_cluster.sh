c4a09acb1d32b48f5017f1339e08627b0ea02a30
sl1msn1per <sl1msn1per@live.com>
2017-05-17 19:12:28 +0000

    cluster script

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! $# -eq 2 ]
+if [ ! $# -eq 1 ]
 then
 	echo "Usage:   $0 num_processes random_image_size"
 	echo "Example: $0 8 1024"
@@ -8,7 +8,7 @@ then
 fi
 p=$1
 nodes=$((($p+3-1)/3));
-w=$2
+w="20"
 jobname="mpi_ccl_${p}_${w}"
 echo "Starting job ${jobname}..."
 dir=$(pwd)
@@ -18,9 +18,14 @@ cat <<EOS | qsub -
 #PBS -N $jobname
 #PBS -e job.err
 #PBS -o job.log
-#PBS -l walltime=0:01:00
+#PBS -l walltime=0:05:00
 #PBS -q batch
 #PBS -l nodes=$nodes:ppn=3
 cd $dir
-mpirun -np $p ./ccl_mpi -m random -w $w -b >> "mpi${p}.out"
+make
+while [ $w -lt 2025 ]
+do
+	mpirun -np $p ./ccl_mpi -m random -w $w -b >> "mpi${p}.out"
+	w=$[$w+20]
+done
 EOS
